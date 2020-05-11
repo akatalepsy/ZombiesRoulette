9ca5129978f2771f2aad0a4127e61a2ab4494b11
Sly • スライ <63747478+slydog111@users.noreply.github.com>
2020-05-11 19:55:28 -0400

    Add files via upload

---
+++ kino.py
@@ -0,0 +1,163 @@
+def weapons():
+    map_weapons = {
+        0: "M1911",
+        1: "Olympia",
+        2: "M14",
+        3: "PM63",
+        4: "MPL",
+        5: "MP5k",
+        6: "AK74u",
+        7: "MP40",
+        8: "M16",
+        9: "Stakeout",
+        10: "CZ75",
+        11: "CZ75 Dual Wield",
+        12: "Python",
+        13: "Spectre",
+        14: "FN FAL",
+        15: "G11",
+        16: "Famas",
+        17: "AUG",
+        18: "Commando",
+        19: "Galil",
+        20: "HS-10",
+        21: "SPAS-12",
+        22: "Dragunov",
+        23: "L96A1",
+        24: "RPK",
+        25: "HK21",
+        26: "Ballistic Knife",
+        27: "Crossbow",
+        28: "China Lake",
+        29: "M72 LAW",
+        30: "Ray Gun",
+        31: "Thundergun"
+    }
+
+    return map_weapons
+
+
+def weapon_categories(category):
+    all_weapons = range(0, 32)
+
+    starting_weapons = 0
+    wall_weapons = range(1, 10)
+    box_weapons = range(10, 32)
+
+    handguns = (0, 10, 11, 12)
+    submachine_guns = (*range(3, 8), 13)
+    assault_rifles = (2, 8, *range(14, 20))
+    shotguns = (1, 9, 20, 21)
+    sniper_rifles = (22, 23)
+    light_machine_guns = (24, 25)
+    special_weapons = (26, 27)
+    heavy_weapons = (28, 29)
+    wonder_weapons = (30, 31)
+
+    explosive_weapons = range(27, 31)
+
+    categories = {
+        0: all_weapons,
+        1: starting_weapons,
+        2: wall_weapons,
+        3: box_weapons,
+        4: handguns,
+        5: submachine_guns,
+        6: assault_rifles,
+        7: shotguns,
+        8: sniper_rifles,
+        9: light_machine_guns,
+        10: special_weapons,
+        11: heavy_weapons,
+        12: wonder_weapons,
+        13: explosive_weapons
+    }
+
+    category_list = f'''
+    0 = All weapons ({len(all_weapons)})
+    
+    Location:
+    1 = Starting weapons (1)
+    2 = Wall weapons ({len(wall_weapons)})
+    3 = Box weapons ({len(box_weapons)})
+    
+    Weapon Type:
+    4 = Handguns ({len(handguns)})
+    5 = Submachine guns ({len(submachine_guns)})
+    6 = Assault Rifles ({len(assault_rifles)})
+    7 = Shotguns ({len(shotguns)})
+    8 = Sniper rifles ({len(sniper_rifles)})
+    9 = Light machine guns ({len(light_machine_guns)})
+    10 = Special weapons ({len(special_weapons)})
+    11 = Heavy weapons ({len(heavy_weapons)})
+    12 = Wonder weapons ({len(wonder_weapons)})
+
+    Other Categories:
+    13 = Explosive weapons ({len(explosive_weapons)})
+'''
+
+    l = category_list
+
+    if type(eval(category)) == int:
+        return categories[int(category)]
+    else:
+        return eval(category)
+
+
+def perks():
+    map_perks = {
+        1: "Jugger-Nog",
+        2: "Quick Revive",
+        3: "Double Tap Root Beer",
+        4: "Speed Cola",
+        5: "Mule Kick"
+    }
+
+    return map_perks
+
+
+def perk_categories(category):
+    all_perks = range(1, 6)
+
+    alley_side_perks = range(1, 4)
+    dressing_room_side_perks = (1, 2, 4, 5)
+
+    speed_related_perks = range(2, 5)
+    health_related_perks = (1, 2)
+    weapon_related_perks = (3, 4, 5)
+
+    categories = {
+        0: all_perks,
+        1: alley_side_perks,
+        2: dressing_room_side_perks,
+        3: speed_related_perks,
+        4: health_related_perks,
+        5: weapon_related_perks
+    }
+
+    category_list = f'''
+    0 = All perks ({len(all_perks)})
+    
+    Location:
+    1 = Alley side ({len(alley_side_perks)})
+    2 = Dressing Room side ({len(dressing_room_side_perks)})
+    
+    Function:
+    3 = Speed ({len(speed_related_perks)})
+    4 = Health ({len(health_related_perks)})
+    5 = Weapon ({len(weapon_related_perks)})
+'''
+
+    l = category_list
+
+    if type(eval(category)) == int:
+        return categories[int(category)]
+    else:
+        return eval(category)
+
+
+def category_range_list(category):
+    weapon = 14
+    perk = 6
+
+    return eval(category)
---
+++ moon.py
@@ -0,0 +1,171 @@
+def weapons():
+    map_weapons = {
+        0: "M1911",
+        1: "M14",
+        2: "Olympia",
+        3: "PM63",
+        4: "MPL",
+        5: "MP5k",
+        6: "AK74u",
+        7: "M16",
+        8: "Stakeout",
+        9: "CZ75",
+        10: "CZ75 Dual Wield",
+        11: "Python",
+        12: "Spectre",
+        13: "FN FAL",
+        14: "G11",
+        15: "Famas",
+        16: "AUG",
+        17: "Commando",
+        18: "Galil",
+        19: "HS-10",
+        20: "SPAS-12",
+        21: "Dragunov",
+        22: "L96A1",
+        23: "RPK",
+        24: "HK21",
+        25: "Ballistic Knife",
+        26: "Crossbow",
+        27: "China Lake",
+        28: "M72 LAW",
+        29: "Ray Gun",
+        30: "Wave Gun"
+    }
+
+    return map_weapons
+
+
+def weapon_categories(category):
+    all_weapons = range(0, 31)
+
+    starting_weapons = 0
+    wall_weapons = range(1, 9)
+    box_weapons = range(9, 31)
+
+    handguns = (0, 9, 10, 11)
+    submachine_guns = (*range(3, 7), 12)
+    assault_rifles = (1, 7, *range(13, 19))
+    shotguns = (2, 8, 19, 20)
+    sniper_rifles = (21, 22)
+    light_machine_guns = (23, 24)
+    special_weapons = (25, 26)
+    heavy_weapons = (27, 28)
+    wonder_weapons = (29, 30)
+
+    explosive_weapons = range(26, 30)
+
+    categories = {
+        0: all_weapons,
+        1: starting_weapons,
+        2: wall_weapons,
+        3: box_weapons,
+        4: handguns,
+        5: submachine_guns,
+        6: assault_rifles,
+        7: shotguns,
+        8: sniper_rifles,
+        9: light_machine_guns,
+        10: special_weapons,
+        11: heavy_weapons,
+        12: wonder_weapons,
+        13: explosive_weapons
+    }
+
+    category_list = f'''
+    0 = All weapons ({len(all_weapons)})
+    
+    Location:
+    1 = Starting weapons (1)
+    2 = Wall weapons ({len(wall_weapons)})
+    3 = Box weapons ({len(box_weapons)})
+    
+    Weapon Type:
+    4 = Handguns ({len(handguns)})
+    5 = Submachine guns ({len(submachine_guns)})
+    6 = Assault Rifles ({len(assault_rifles)})
+    7 = Shotguns ({len(shotguns)})
+    8 = Sniper rifles ({len(sniper_rifles)})
+    9 = Light machine guns ({len(light_machine_guns)})
+    10 = Special weapons ({len(special_weapons)})
+    11 = Heavy weapons ({len(heavy_weapons)})
+    12 = Wonder weapons ({len(wonder_weapons)})
+
+    Other Categories:
+    13 = Explosive weapons ({len(explosive_weapons)})
+'''
+
+    l = category_list
+
+    if type(eval(category)) == int:
+        return categories[int(category)]
+    else:
+        return eval(category)
+
+
+def perks():
+    map_perks = {
+        1: "Jugger-Nog",
+        2: "Quick Revive",
+        3: "Speed Cola",
+        4: "Double Tap Root Beer",
+        5: "PhD Flopper",
+        6: "Stamin-Up",
+        7: "Deadshot Daiquiri",
+        8: "Mule Kick"
+    }
+
+    return map_perks
+
+
+def perk_categories(category):
+    all_perks = range(1, 9)
+
+    area_51_perks = (1, 3)
+    rec_and_tunnel_perks = (2, 6)
+    lab_perks = (4, 7)
+    bio_dome_perks = (5, 8)
+
+    speed_related_perks = (2, 3, 4, 6)
+    health_related_perks = (1, 2)
+    weapon_related_perks = (3, 4, 7)
+
+    categories = {
+        0: all_perks,
+        1: area_51_perks,
+        2: rec_and_tunnel_perks,
+        3: lab_perks,
+        4: bio_dome_perks,
+        5: speed_related_perks,
+        6: health_related_perks,
+        7: weapon_related_perks
+    }
+
+    category_list = f'''
+    0 = All perks ({len(all_perks)})
+    
+    Location:
+    1 = Area 51 ({len(area_51_perks)})
+    2 = Receiving Bay and Tunnels ({len(rec_and_tunnel_perks)})
+    3 = Laboratories ({len(lab_perks)})
+    4 = Bio Dome area ({len(bio_dome_perks)})
+
+    Function:
+    5 = Speed ({len(speed_related_perks)})
+    6 = Health ({len(health_related_perks)})
+    7 = Weapon ({len(weapon_related_perks)})
+'''
+
+    l = category_list
+
+    if type(eval(category)) == int:
+        return categories[int(category)]
+    else:
+        return eval(category)
+
+
+def category_range_list(category):
+    weapon = 14
+    perk = 8
+
+    return eval(category)
